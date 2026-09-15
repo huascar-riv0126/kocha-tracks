af00f9d083147da7c6ef3939179ac44982293682
huascar-riv0126 <huascar.rivero.dev0126@gmail.com>
2026-09-15 03:20:57 -0400

    feat: navbar sin textos hard-coded

--- angular-frontend/src/app/layout/navbar/navbar.ts
+++ angular-frontend/src/app/layout/navbar/navbar.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { GetStringsPipe } from '../../core/strings/get-strings-pipe';
 
 @Component({
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, GetStringsPipe],
   selector: 'app-navbar',
   styleUrl: './navbar.css',
   templateUrl: './navbar.html',
