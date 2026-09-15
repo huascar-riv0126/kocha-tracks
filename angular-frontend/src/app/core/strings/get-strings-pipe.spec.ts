91e842319d8451335d94d571baaecbb4f3f8f5ba
huascar-riv0126 <huascar.rivero.dev0126@gmail.com>
2026-09-15 03:02:37 -0400

    feat: pipe personalizado para sacar los textos

---
+++ angular-frontend/src/app/core/strings/get-strings-pipe.spec.ts
@@ -0,0 +1,8 @@
+import { GetStringsPipePipe } from './get-strings-pipe-pipe';
+
+describe('GetStringsPipePipe', () => {
+  it('create an instance', () => {
+    const pipe = new GetStringsPipePipe();
+    expect(pipe).toBeTruthy();
+  });
+});
