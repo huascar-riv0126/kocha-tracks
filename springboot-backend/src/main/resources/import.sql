b71fa6b8d9e84366748a787488c09a1c348512f2
jherson77u7 <chambimauricio7378@gmail.com>
2026-09-22 18:12:54 -0400

    implement UH-18 backed

---
+++ springboot-backend/src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO users (name, email, password, active) VALUES ('Jherson Dev', 'jherson@kochatracks.com', '123456', true);
+INSERT INTO users (name, email, password, active) VALUES ('Maria Lopez', 'maria@kochatracks.com', '123456', true);
+INSERT INTO users (name, email, password, active) VALUES ('Usuario Inactivo', 'inactivo@kochatracks.com', '123456', false);
