0d7569ed3a8b434310d5f3865fb7da0b57beb24b
Griz-8 <michelle.carvajal@ucb.edu.bo>
2026-09-24 18:29:15 -0400

    feat(sidebar): añadir dropdown  9 departamentos en historicos defensa sprint 2

--- angular-frontend/src/app/map/map-view/components/sidebar-reports/sidebar-reports.ts
+++ angular-frontend/src/app/map/map-view/components/sidebar-reports/sidebar-reports.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms'; // <-- 1. Importar FormsModule
 import { EventMockService } from '../../../../event/event-mock';
 import { EventMock } from '../../../../event/event-mock.model';
 import { ReportCardComponent } from '../report-card/report-card';
@@ -7,7 +8,7 @@ import { ReportCardComponent } from '../report-card/report-card';
 @Component({
   selector: 'app-sidebar-reports',
   standalone: true,
-  imports: [CommonModule, ReportCardComponent],
+  imports: [CommonModule, FormsModule, ReportCardComponent], // <-- 2. Agregar FormsModule
   templateUrl: './sidebar-reports.html',
   styleUrl: './sidebar-reports.css'
 })
@@ -17,6 +18,20 @@ export class SidebarReportsComponent implements OnInit {
   activeTab: 'activos' | 'historicos' = 'activos';
   selectedCategory: string = 'Todo';
   selectedEventId: number | null = 1;
+
+  // Dropdown de Departamentos para Históricos
+  selectedDepartment: string = 'Cochabamba';
+  departments: string[] = [
+    'Beni',
+    'Chuquisaca',
+    'Cochabamba',
+    'La Paz',
+    'Oruro',
+    'Pando',
+    'Potosí',
+    'Santa Cruz',
+    'Tarija'
+  ];
 
   categories: string[] = [
     'Todo', 'Inundación', 'Incendio', 'Accidente', 'Corte de vía', 'Protesta', 'Otro'
@@ -43,6 +58,10 @@ export class SidebarReportsComponent implements OnInit {
 
   selectCategory(category: string): void {
     this.selectedCategory = category;
+  }
+
+  onDepartmentChange(dept: string): void {
+    this.selectedDepartment = dept;
   }
 
   onSelectEvent(event: EventMock): void {
